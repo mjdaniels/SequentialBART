1e3c4350d2b3a2e971d793e6fa4a1dbd903a3ba2
mjdaniels <mjdaniels@austin.utexas.edu>
2017-02-19 15:42:46 -0600

    adding data

---
+++ R/DataDocumentation.R
@@ -0,0 +1,19 @@
+#' Data values for variable xx for the Seq BART function
+#' A dummy dataset to run the function Seq BART on.
+#' @format This dataframe has 150 rows and 4 columns having missing values at random.
+"xx"
+
+#' Data values for variable y (continuous) for the Seq BART function
+#' A dummy dataset to run the function Seq BART on.
+#' @format This dataframe has 150 values.
+"y1"
+
+#' Data values for variable y (binary) for the Seq BART function
+#' A dummy dataset to run the function Seq BART on.
+#' @format This dataframe has 150 values.
+"y2"
+
+#' Data values for dataypes
+#' A dummy dataset to run the function Seq BART on.
+#' @format This dataframe has 4 rows
+"datatype"
