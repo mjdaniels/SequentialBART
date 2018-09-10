9af14500a96908785b756c1c3b2be7b4f922a668
Mike Daniels <mjdaniels@austin.utexas.edu>
2018-09-10 10:46:46 -0400

    Update mi-bart-y1.cpp

--- src/mi-bart-y1.cpp
+++ src/mi-bart-y1.cpp
@@ -28,7 +28,7 @@ using namespace Eigen;
 //--------------------------------
 // main program
 //--------------------------------
- [[Rcpp::export]]
+// [[Rcpp::export]]
 NumericVector cpp_bart_y1 (NumericVector new_xroot, NumericVector new_yroot, int new_nd, int new_burn,
                            int new_m, int new_nu, int new_kfac, int  new_nmissing,
                            IntegerVector new_xmissroot, int new_bistart, NumericVector new_vartyperoot,
